b0ce29877a677d7e3923edbc1c741b78b9ac35ba
Songhai <95398822+SonghaiFan@users.noreply.github.com>
2025-08-15 01:48:07 +1000

    Remove topic visualization utility functions and related types from topic-visual.utils.ts

--- visualisation_new/topic/topic-config.ts
+++
@@ -1,8 +0,0 @@
-import { merge } from "lodash";
-import { SHARED_CONFIG } from "@/components/visualisation/shared/visualization-config";
-
-export const TOPIC_CONFIG = merge({}, SHARED_CONFIG, {
-  margin: {
-    right: 60, // Reduced right margin compared to shared config
-  },
-});
